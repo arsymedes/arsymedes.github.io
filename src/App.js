98d83165fd36f0e32126770bfb18a912d3f38bfc
Ahmad Arsy <arsyahmad14@gmail.com>
2023-01-19 04:43:57 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black text-white px-8">
+      <section className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold tracking-wide mb-2">Ahmad Arsy</h1>
+        <h2 className="text-2xl font-semibold">Front End Web Developer</h2>
+        <p></p>
+      </section>
+      <section className="min-h-screen flex flex-col items-center">
+        <h3 className="text-5xl font-bold">Projects</h3>
+      </section>
     </div>
   );
 }
