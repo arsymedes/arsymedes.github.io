8f94a5c880cd052f324f9c73153c3cff444cb721
Ahmad Arsy <arsyahmad14@gmail.com>
2023-01-25 23:21:43 +0700

    Finished Her Design

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -18,18 +18,29 @@ function Hero() {
   // }, []);
 
   return (
-    <section className="h-screen grid place-items-center pt-10 relative z-0">
-      <div className="text-4xl font-semibold relative">
-        <h1>Hello, I'm Arsy</h1>
-        <h1>
-          <span>A </span>
-          <ReactTextTransition>
-            <span className="text-[#24ece7]">{texts[count % texts.length].split(" ")[0]}</span>
+    <section className="h-screen grid items-center p-[10vw] sm:p-32 z-0">
+      <div className="text-7xl sm:text-8xl text-gray-700 font-medium flex flex-col gap-6">
+        <h1>Hello<span className="text-red-600">.</span></h1>
+        <h1>I'm Arsy</h1>
+        <h1 className="relative font-semibold w-[105%]">
+          <ReactTextTransition className="absolute -top-2 left-2 text-[#24ece7] select-none">
+            <span>{texts[count % texts.length].split(" ")[0]}</span>
             <span> </span>
-            <span className="text-[#f7004d]">{texts[count % texts.length].split(" ")[1]}</span>
+            <span>{texts[count % texts.length].split(" ")[1]}</span>
+          </ReactTextTransition>
+          <ReactTextTransition className="absolute top-2 -left-2 text-[#f7004d] select-none">
+            <span>{texts[count % texts.length].split(" ")[0]}</span>
+            <span> </span>
+            <span>{texts[count % texts.length].split(" ")[1]}</span>
+          </ReactTextTransition>
+          <ReactTextTransition className="text-gray-900">
+            <span>{texts[count % texts.length].split(" ")[0]}</span>
+            <span> </span>
+            <span>{texts[count % texts.length].split(" ")[1]}</span>
           </ReactTextTransition>
         </h1>
       </div>
+      <div className="absolute w-60 h-60 bg-yellow-300 -z-10 right-[300px]"></div>
     </section>
   );
 }
