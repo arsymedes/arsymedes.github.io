1573190c192cb36e27080b426370cfbdc6dc731b
Ahmad Arsy <arsyahmad14@gmail.com>
2023-01-26 19:56:35 +0700

    Added responsiveness on about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,36 +2,39 @@ import React from "react";
 
 function About() {
   return(
-    <section className="sm:px-32 pb-8">
-      <h2 className="sm:text-7xl font-semibold text-gray-800">About</h2>
-      <div className="flex flex-col gap-4 pt-10 pb-8 text-xl">
-        <p>My name is Ahmad Arsy, and I'm a solution-driven Full Stack Web Developer with 1 year of learning experience currently pursuing a degree in Physics from University of Indonesia</p>
-        <p>Over the past year, software engineering has developed to be a passion of mine, due to the problem solving nature of it. React, Tailwind CSS, Python, and Firebase are some of the things I'm proficient at. I am excited to collaborate on a team and apply my skills to develop applications, and I'm open to learn new things along the way. </p>
-      </div>
-      <div className="flex flex-wrap sm:gap-12">
-        <div>
-          <h3 className="text-3xl font-semibold text-gray-800">Front End</h3>
-          <ul className="text-2xl pt-4 text-gray-700">
-            <li>React</li>
-            <li>Tailwind CSS</li>
-            <li>Jest</li>
-          </ul>
+    <section className="p-12 md:px-32 pb-8">
+      <h2 className="text-7xl font-semibold pb-12 text-gray-800">About</h2>
+      <div className="grid place-items-center md:block">
+        <div className="w-60 md:min-w-[15rem] h-60 md:h-96 box-border bg-rose-600 shadow-teal-300 shadow-[12px_12px_0px_0px] md:float-left mr-12"></div>
+        <div className="flex flex-col gap-4 text-xl pt-12 md:pt-0 pb-12">
+          <p>My name is Ahmad Arsy, and I'm a solution-driven Full Stack Web Developer with 1 year of learning experience currently pursuing a degree in Physics from University of Indonesia</p>
+          <p>Over the past year, software engineering has developed to be a passion of mine, due to the problem solving nature of it. React, Python, and Firebase are some of the things I'm proficient at. I am excited to collaborate on a team and apply my skills to develop applications, and I'm open to learn new things along the way. </p>
         </div>
-        <div>
-          <h3 className="text-3xl font-semibold text-gray-800">Back End</h3>
-          <ul className="text-2xl pt-4 text-gray-700">
-            <li>Firebase</li>
-            <li>Node JS</li>
-            <li>SQL</li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="text-3xl font-semibold text-gray-800">Dev Tools</h3>
-          <ul className="text-2xl pt-4 text-gray-700">
-            <li>Git</li>
-            <li>NPM</li>
-            <li>Python</li>
-          </ul>
+        <div className="flex flex-wrap gap-5 sm:gap-10">
+          <div>
+            <h3 className="text-3xl font-semibold text-gray-800">Front End</h3>
+            <ul className="text-2xl pt-4 text-gray-700">
+              <li>React</li>
+              <li>Tailwind CSS</li>
+              <li>Jest</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-3xl font-semibold text-gray-800">Back End</h3>
+            <ul className="text-2xl pt-4 text-gray-700">
+              <li>Firebase</li>
+              <li>Node JS</li>
+              <li>SQL</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-3xl font-semibold text-gray-800">Dev Tools</h3>
+            <ul className="text-2xl pt-4 text-gray-700">
+              <li>Git</li>
+              <li>NPM</li>
+              <li>Python</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
