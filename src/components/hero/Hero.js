bbbde3e317b0897c1aa84ed508cd417782c7cbc8
Ahmad Arsy <arsyahmad14@gmail.com>
2023-02-14 22:43:43 +0700

    Fixed interval bug and improve responsiveness

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,13 +8,13 @@ function Hero() {
   const texts = ["Web Developer", "ML Engineer", "Physics Scholar"];
 
   useEffect(() => {
-    function timeout() {
-      setTimeout(() => {
-        setCount((prev) => prev + 1);
-        timeout()
-      }, 4000);
-    }
-    timeout()
+    const myInterval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 4000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
   }, []);
 
   const h1Animate = {
@@ -25,11 +25,22 @@ function Hero() {
   const boxAnimate = {
     off: { x: "100vw" },
     on: { x: 0, transition: { duration: 0.8 } },
-  }
+  };
 
   return (
-    <motion.section initial="off" whileInView="on" viewport={{once: true}} className="h-screen grid items-center z-0">
-      <motion.div initial="off" whileInView="on" viewport={{once: true}} transition={{staggerChildren: 0.75}} className="text-7xl sm:text-8xl text-gray-700 font-medium z-10 flex flex-col gap-6">
+    <motion.section
+      initial="off"
+      whileInView="on"
+      viewport={{ once: true }}
+      className="h-screen grid items-center z-0"
+    >
+      <motion.div
+        initial="off"
+        whileInView="on"
+        viewport={{ once: true }}
+        transition={{ staggerChildren: 0.75 }}
+        className="text-6xl sm:text-8xl text-gray-700 font-medium z-10 flex flex-col gap-6"
+      >
         <motion.h1 variants={h1Animate}>
           Hello<span className="text-red-600">.</span>
         </motion.h1>
@@ -43,7 +54,10 @@ function Hero() {
           </ReactTextTransition>
         </motion.h1>
       </motion.div>
-      <motion.div variants={boxAnimate} className="absolute w-60 h-60 bg-yellow-300 shadow-[-12px_-12px] shadow-pink-500 -right-32 sm:right-[300px]"></motion.div>
+      <motion.div
+        variants={boxAnimate}
+        className="absolute w-60 h-60 bg-yellow-300 shadow-[-12px_-12px] shadow-pink-500 -right-32 sm:right-[300px]"
+      ></motion.div>
     </motion.section>
   );
 }
